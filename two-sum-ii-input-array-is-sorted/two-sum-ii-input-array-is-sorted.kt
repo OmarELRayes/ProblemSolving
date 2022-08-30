946c654791775db4c8425ed0371192b0dc95570f
Omar ELRayes <omarelreas@gmail.com>
2022-08-30 10:03:59 +0200

    Time: 322 ms, Memory: 46.6 MB - LeetHub

---
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+        var i = 0
+        var j = numbers.size - 1
+        while (i < j) {
+            val sum = numbers[i] + numbers[j]
+            if (sum == target) return intArrayOf(i + 1, j + 1)
+            else if (sum > target) j--
+            else i++
+        }
+        return intArrayOf()
+    }
+}
